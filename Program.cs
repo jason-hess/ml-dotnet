cdf9eff513e3d0cc76aa79ad2174e7d46e18bfaa
Jason Hess <jasonehess@hotmail.com>
2018-10-31 11:21:07 +1000

    add csproj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ml_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
