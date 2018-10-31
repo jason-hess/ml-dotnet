e061bad0c3b9165cf5067502e63c67cd2c719e1a
Jason Hess <jasonehess@hotmail.com>
2018-10-31 11:27:08 +1000

    initial skeleton

--- Program.cs
+++ Program.cs
@@ -1,12 +1,95 @@
-﻿using System;
+﻿using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Runtime.Data;
+using Microsoft.ML.Runtime.Learners;
+using System;
 
 namespace ml_dotnet
 {
-    class Program
+    public class Program
     {
+
+        // STEP 1: Define your data structures
+        // IrisData is used to provide training data, and as
+        // input for prediction operations
+        // - First 4 properties are inputs/features used to predict the label
+        // - Label is what you are predicting, and is only set when training
+        public class IrisData
+        {
+            [Column("0")]
+            public float SepalLength;
+
+            [Column("1")]
+            public float SepalWidth;
+
+            [Column("2")]
+            public float PetalLength;
+
+            [Column("3")]
+            public float PetalWidth;
+
+            [Column("4")]
+            [ColumnName("Label")]
+            public string Label;
+        }
+
+        // IrisPrediction is the result returned from prediction operations
+        public class IrisPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // STEP 2: Create an evironment  and load your data
+            var env = new LocalEnvironment();
+
+            // If working in Visual Studio, make sure the 'Copy to Output Directory'
+            // property of iris-data.txt is set to 'Copy always'
+            string dataPath = "iris-data.txt";
+            var reader = new TextLoader(env,
+                new TextLoader.Arguments()
+                {
+                    Separator = ",",
+                    HasHeader = true,
+                    Column = new[]
+                    {
+                            new TextLoader.Column("SepalLength", DataKind.R4, 0),
+                            new TextLoader.Column("SepalWidth", DataKind.R4, 1),
+                            new TextLoader.Column("PetalLength", DataKind.R4, 2),
+                            new TextLoader.Column("PetalWidth", DataKind.R4, 3),
+                            new TextLoader.Column("Label", DataKind.Text, 4)
+                    }
+                });
+
+            IDataView trainingDataView = reader.Read(new MultiFileSource(dataPath));
+
+            // STEP 3: Transform your data and add a learner
+            // Assign numeric values to text in the "Label" column, because only
+            // numbers can be processed during model training.
+            // Add a learning algorithm to the pipeline. e.g.(What type of iris is this?)
+            // Convert the Label back into original text (after converting to number in step 3)
+            var pipeline = new TermEstimator(env, "Label", "Label")
+                   .Append(new ConcatEstimator(env, "Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                   .Append(new SdcaMultiClassTrainer(env, new SdcaMultiClassTrainer.Arguments()))
+                   .Append(new KeyToValueEstimator(env, "PredictedLabel"));
+
+            // STEP 4: Train your model based on the data set  
+            var model = pipeline.Fit(trainingDataView);
+
+            // STEP 5: Use your model to make a prediction
+            // You can change these numbers to test different predictions
+            var prediction = model.MakePredictionFunction<IrisData, IrisPrediction>(env).Predict(
+                new IrisData()
+                {
+                    SepalLength = 3.3f,
+                    SepalWidth = 1.6f,
+                    PetalLength = 0.2f,
+                    PetalWidth = 5.1f,
+                });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
         }
     }
+
 }
