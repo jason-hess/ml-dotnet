0aa41f407662820532b2974bbc02b540f9c51c31
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-07-19 17:15:29 +1000

    add user classification

---
+++ UserClassification/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace UserClassification
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/machine-learning/tutorials/sentiment-analysis
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+            var mlContext = new MLContext();
+            var splitDataView = LoadData(mlContext);
+            var model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            Evaluate(mlContext, model, splitDataView.TestSet);
+            //UseModelWithSingleItem(mlContext, model);
+            Console.WriteLine("Please Type Your Review:");
+            var review = Console.ReadLine();
+            UseModelWithSingleItem(mlContext, model, review);
+            //UseModelWithBatchItems(mlContext, model);
+        }
+
+        public static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+                    SentimentText = "This was a horrible meal"
+                },
+                new SentimentData
+                {
+                    SentimentText = "I love this spaghetti."
+                }
+            };
+            var batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+            var predictions = model.Transform(batchComments);
+
+            // Use model to predict whether comment data is Positive (1) or Negative (0).
+            var predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+
+            foreach (SentimentPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+        }
+
+        private static void UseModelWithSingleItem(MLContext mlContext, ITransformer model, string review)
+        {
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            var sampleStatement = new SentimentData
+            {
+                SentimentText = review
+            };
+            var resultPrediction = predictionFunction.Predict(sampleStatement);
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+        }
+
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            var predictions = model.Transform(splitTestSet);
+            var metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+
+        private static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(trainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            return model;
+        }
+
+        private static DataOperationsCatalog.TrainTestData LoadData(MLContext mlContext)
+        {
+            var dataView =
+                mlContext.Data.LoadFromTextFile<SentimentData>(
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "training-2.txt"), hasHeader: false);
+            return mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+        }
+    }
+
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
+    }
+
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
